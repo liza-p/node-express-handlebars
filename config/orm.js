7a383078737d58ba4e293c4c4d64905eadd46756
liza p <liyourlp@gmail.com>
2020-07-26 20:20:07 -0700

    created orm update function

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,28 @@
 const connection = require("./connection.js");
 
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -31,6 +54,19 @@ const orm = {
       cb(result);
     });
   },
+  update: function(table, objColVals, cd){
+    var queryString = `UPDATE ${table} 
+    SET ${objToSql(objColVals)}
+    WHERE ${condition}`
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
 };
 
 module.exports = orm;
